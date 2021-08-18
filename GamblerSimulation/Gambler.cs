8b9b4608f20807a8c9ca85a5621bf5d53d473640
bharathmighty248 <bharathmighty248@gmail.com>
2021-08-18 09:40:46 +0530

    [Bharath] Add: EveryDay Stack And Bet Implemented

---
+++ GamblerSimulation/Gambler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamblerSimulation
+{
+    class Gambler
+    {
+        public void Game()
+        {
+            int stack = 100;
+            int bet = 1;
+            Console.WriteLine("Every Day Stack is " + stack);
+            Console.WriteLine("EveryTime Bet is " + bet);
+        }
+    }
+}
---
+++ GamblerSimulation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GamblerSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gambler gambler = new Gambler();
+            gambler.Game();
+        }
+    }
+}
